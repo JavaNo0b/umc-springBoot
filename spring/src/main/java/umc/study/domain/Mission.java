9c1241c3ca9837b7ec7d1641bca7d237cba2a714
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-05-20 00:35:55 +0900

    entity

---
+++ spring/src/main/java/umc/study/domain/Mission.java
@@ -0,0 +1,27 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.enums.Gender;
+import umc.study.domain.enums.MemberStatus;
+import umc.study.domain.enums.SocialType;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer reward;
+
+    private LocalDate deadline;
+
+    private String missionSpec;
+}
