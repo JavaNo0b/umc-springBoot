561badde7fd19ef4154b6f38813ed222012c5678
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-06-17 00:43:18 +0900

    페이징 성공 (커스텀 어노테이션 아직 안함)

--- spring/src/main/java/umc/study/web/controller/StoreRestController.java
+++ spring/src/main/java/umc/study/web/controller/StoreRestController.java
@@ -1,11 +1,20 @@
 package umc.study.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.StoreConverter;
+import umc.study.domain.Review;
 import umc.study.domain.Store;
+import umc.study.service.StoreQueryService;
 import umc.study.service.StoreService;
 import umc.study.validation.annotation.ExistStore;
 import umc.study.web.dto.StoreRequestDTO;
@@ -17,11 +26,33 @@ import umc.study.web.dto.StoreResponseDTO;
 public class StoreRestController {
 
     private final StoreService storeService;
+    private final StoreQueryService storeQueryService;
 
     @PostMapping("/")
     public ApiResponse<StoreResponseDTO.JoinStoreResultDTO> joinStore(@RequestParam Long regionId,
                                                                  @RequestBody @Valid StoreRequestDTO.JoinStoreDTO request) {
         Store newStore = storeService.joinStore(request, regionId);
         return ApiResponse.onSuccess(StoreConverter.toJoinStoreResultDTO(newStore));
+    }
+
+    //@Operation은 이 API에 대한 설명을 넣게 되며 summary, description으로 설명을 적습니다.
+    //@ApiResponses로 이 API의 응답을 담게 되며 내부적으로 @ApiResponse로 각각의 응답들을 담게 됩니다.
+    //@Parameters 는 프론트엔드에서 넘겨줘야 할 정보를 담으며, 위의 코드에선 일단 path variable만 기재했고, API 완성 단계에서 query String도 추가할 것입니다.
+    @GetMapping("/{storeId}/reviews")
+    @Operation(summary = "특정 가게의 리뷰 목록 조회 API",description = "특정 가게의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "페이지 번호, 0번이 1 페이지 입니다."),
+    })
+    public ApiResponse<StoreResponseDTO.ReviewPreViewListDTO> getReviewList(@ExistStore @PathVariable(name = "storeId") Long storeId,
+                                                                            @RequestParam(name = "page") Integer page) {
+        Page<Review> StorePage = storeQueryService.getReviewList(storeId, page);
+        return ApiResponse.onSuccess(StoreConverter.toReviewPreViewListDTO(StorePage));
     }
 }
--- spring/src/main/java/umc/study/web/dto/StoreResponseDTO.java
+++ spring/src/main/java/umc/study/web/dto/StoreResponseDTO.java
@@ -5,10 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class StoreResponseDTO {
 
+    // 지역에 가게 추가
     @Builder
     @Getter
     @NoArgsConstructor
@@ -16,5 +19,31 @@ public class StoreResponseDTO {
     public static class JoinStoreResultDTO{
         Long storeId;
         LocalDateTime createdAt;
+    }
+
+
+    // 리뷰 목록 리스트 조회(페이징)
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+    // 리뷰 목록
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname; //멤버의 정보가 닉네임 말고도 많이 필요하다면 이렇게도 가능 MemberInfoDTO memberInfo;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
